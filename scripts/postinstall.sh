57f3fc20072d8951828360c1da58f9b1e210c70d
Marc Juul <juul@labitat.dk>
2013-12-12 23:33:15 -0800

    added stuff

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 # Here we can put scripts that should run as part of installing openwrt-firmware-config.
+
+/etc/init.d/telnet disable
+/etc/init.d/tunneldigger disable
+/etc/init.d/nodeconf_client enable
+

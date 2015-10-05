24e32aecba11aa8055931b3e119146f8cad7421b
Maxb <maxb.personal@gmail.com>
2015-10-04 19:04:07 -0700

    Adding sudomesh helper functions to openwrt-firmware-config package.

--- init-scripts/postinstall.sh
+++ init-scripts/postinstall.sh
@@ -3,6 +3,4 @@
 # Here we can put scripts that should run as part of installing openwrt-firmware-config.
 
 /etc/init.d/telnet disable
-/etc/init.d/tunneldigger disable
-/etc/init.d/nodeconf_client enable
 
